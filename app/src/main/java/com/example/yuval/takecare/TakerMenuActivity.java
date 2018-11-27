3d6a3223630c8e0f466e55467cf6d4f08503084b
Saharco <saharco@campus.technion.ac.il>
2018-11-27 04:40:05 +0200

    UI-Complete version of the TakerActivityMenu

--- app/src/main/java/com/example/yuval/takecare/TakerMenuActivity.java
+++ app/src/main/java/com/example/yuval/takecare/TakerMenuActivity.java
@@ -1,11 +1,15 @@
 package com.example.yuval.takecare;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.renderscript.RenderScript;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.SoundEffectConstants;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,21 +19,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-import java.util.Set;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TakerMenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     MenuItem[] prevNavGroupItem;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taker_menu);
+
+        //Set the toolbar as the AppBar for this activity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Set up the onClick listener for the giver form button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +54,7 @@ public class TakerMenuActivity extends AppCompatActivity
             }
         });
 
+        //Set up the navigation drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -48,6 +64,8 @@ public class TakerMenuActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Initialize three default checked items from the drawer
+        //Manipulating these items ensures that one item is always checked from each menu
         prevNavGroupItem = new MenuItem[3];
         prevNavGroupItem[0] = navigationView.getMenu().findItem(R.id.nav_feed_display);
         prevNavGroupItem[0].setChecked(true);
@@ -55,7 +73,43 @@ public class TakerMenuActivity extends AppCompatActivity
         prevNavGroupItem[1].setChecked(true);
         prevNavGroupItem[2] = navigationView.getMenu().findItem(R.id.nav_any_pickup);
         prevNavGroupItem[2].setChecked(true);
+
+        setUpRecyclerView(recyclerView);
     }
+
+    private void setUpRecyclerView(RecyclerView recyclerView) {
+        recyclerView = (RecyclerView) findViewById(R.id.taker_feed_list);
+        //Optimizing recycler view's performance:
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemViewCacheSize(10);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+
+        //TODO: properly implement factory with input stream
+        /*InputStream is = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 8;
+        Bitmap preview_bitmap = BitmapFactory.decodeStream(is, null, options);*/
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<FeedCardInformation> cards = initData();
+
+        adapter = new TakerRVAdapter(cards);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<FeedCardInformation> initData() {
+        List<FeedCardInformation> list = new ArrayList<>();
+        list.add(new FeedCardInformation("Yummy Muffins For All!", R.drawable.photo_muffin, R.drawable.photo_mcgiverface, "Giver McGiverFace", R.drawable.ic_pizza_slice_purple, R.drawable.ic_giveaway_purple));
+        list.add(new FeedCardInformation("Driving to Tel-Aviv at Approx 7pm", R.drawable.photo_hittchhiker, R.drawable.ic_user_purple, "Israel M. Shalom", R.drawable.ic_car_purple, R.drawable.ic_race_purple));
+        list.add(new FeedCardInformation("I Found An Umbrella Near Ullman", R.drawable.photo_umbrella, R.drawable.ic_user_purple, "Noa", R.drawable.ic_lost_and_found_purple, R.drawable.ic_in_person_purple));
+        list.add(new FeedCardInformation("FREE PIZZAS IN TAUB'S BALCONY!! GET OVER HERE QUICKLY!!", R.drawable.photo_pizza, R.drawable.ic_user_purple, "Yuval", R.drawable.ic_pizza_slice_purple, R.drawable.ic_giveaway_purple));
+        list.add(new FeedCardInformation("This Cool Nightstand!", R.drawable.photo_nightstand, R.drawable.ic_user_purple, "Tzvika", R.drawable.ic_lamp_purple, R.drawable.ic_race_purple));
+        return list;
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -79,12 +133,15 @@ public class TakerMenuActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //TODO: Implement handlers for each user settings item
+        switch(item.getItemId()) {
+            case R.id.action_user_settings: return true;
+            case R.id.action_my_items: return true;
+            case R.id.action_requested_items: return true;
+            case R.id.action_favorites: return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -105,25 +162,69 @@ public class TakerMenuActivity extends AppCompatActivity
         }
         item.setChecked(true);
 
-        //TODO: Implement handlers for each item
-        int id = item.getItemId();
-
-        if (id == R.id.nav_map_display) {
-
-        } else if (id == R.id.nav_show_all) {
-
-        } else if (id == R.id.nav_food) {
-
-        } else if (id == R.id.nav_study_material) {
-
-        } else if (id == R.id.nav_furniture) {
-
-        } else if (id == R.id.nav_lost_and_found) {
-
-        } else if (id == R.id.nav_hitchhike) {
-
+        //TODO: Implement handlers for each drawer item
+        switch(item.getItemId()) {
+            case R.id.nav_map_display: break;
+            case R.id.nav_show_all: break;
+            case R.id.nav_food: break;
+            case R.id.nav_study_material: break;
+            case R.id.nav_furniture: break;
+            case R.id.nav_lost_and_found: break;
+            case R.id.nav_hitchhike: break;
+            case R.id.nav_any_pickup: break;
+            case R.id.nav_in_person: break;
+            case R.id.nav_giveaway: break;
+            case R.id.nav_race: break;
         }
 
         return false;
+    }
+
+    public void onItemCategoryPress(View view) {
+        String str="";
+        if(view.getId() == R.id.item_category) {
+            switch ((int) view.getTag()) {
+                case R.drawable.ic_pizza_slice_purple:
+                    str = "This item's category is food";
+                    break;
+                case R.drawable.ic_book_purple:
+                    str = "This item's category is study material";
+                    break;
+                case R.drawable.ic_lamp_purple:
+                    str = "This item's category is household objects";
+                    break;
+                case R.drawable.ic_lost_and_found_purple:
+                    str = "This item's category is lost&founds";
+                    break;
+                case R.drawable.ic_car_purple:
+                    str = "This item's category is hitchhiking";
+                    break;
+                default:
+                    str = "This item is in a category of its own";
+                    break;
+            }
+        }
+        else {
+            switch ((int) view.getTag()) {
+                case R.drawable.ic_in_person_purple:
+                    str = "This item is available for pick-up in person";
+                    break;
+                case R.drawable.ic_giveaway_purple:
+                    str = "This item is available in a public giveaway";
+                    break;
+                case R.drawable.ic_race_purple:
+                    str = "Race to get this item before anyone else!";
+                    break;
+                default:
+                    break;
+            }
+        }
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onReportPress(View view) {
+        PopupMenu menu = new PopupMenu(this, view);
+        menu.getMenuInflater().inflate(R.menu.report_menu, menu.getMenu());
+        menu.show();
     }
 }
