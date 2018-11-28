afef903260ef9c87dad3b800ac8644a4d0fa9619
Saharco <saharco@campus.technion.ac.il>
2018-11-28 21:42:28 +0200

    ui for user favorites activity finished

---
+++ app/src/main/java/com/example/yuval/takecare/UserFavoritesActivity.java
@@ -0,0 +1,49 @@
+package com.example.yuval.takecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserFavoritesActivity extends AppCompatActivity {
+
+    private FeedRecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_favorites);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.shared_items_toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            //TODO: create intent to move back to taker feed
+            super.onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.taker_menu, menu);
+        return true;
+    }
+}
