8ccde66945ddb1320ea62f9e3bae2ff3c990a621
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2018-11-28 05:33:58 +0200

    added animation to login activity

--- app/src/main/java/com/example/yuval/takecare/LoginActivity.java
+++ app/src/main/java/com/example/yuval/takecare/LoginActivity.java
@@ -1,8 +1,15 @@
 package com.example.yuval.takecare;
 
 import android.graphics.drawable.BitmapDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.transition.Explode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.transition.TransitionManager;
+import android.support.transition.Fade;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,7 +17,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        BitmapDrawable background = new BitmapDrawable();
+
+    }
+
+    public void transition(View view) {
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(this,R.layout.activity_login2);
+        TransitionManager tr = new TransitionManager();
+        TransitionManager.beginDelayedTransition((ConstraintLayout) findViewById(R.id.shit));
+        constraintSet.applyTo((ConstraintLayout) findViewById(R.id.shit));
 
     }
 }
