ed2d49b1faba205e79388718a1a1b3b52f44f3c4
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2018-11-29 03:48:46 +0200

    finished giver menu

---
+++ app/src/main/java/com/example/yuval/takecare/GiverMenu.java
@@ -0,0 +1,13 @@
+package com.example.yuval.takecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GiverMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giver_menu);
+    }
+}
