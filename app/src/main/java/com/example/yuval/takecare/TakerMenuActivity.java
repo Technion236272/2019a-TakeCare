2557422e50c25c2adfc21aca3d42d5924be02c64
Saharco <saharco@campus.technion.ac.il>
2018-12-09 20:50:30 +0200

    added the form logic to develop

--- app/src/main/java/com/example/yuval/takecare/TakerMenuActivity.java
+++ app/src/main/java/com/example/yuval/takecare/TakerMenuActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.SoundEffectConstants;
 import android.view.View;
 import android.support.design.widget.NavigationView;
