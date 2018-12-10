dd74c4b89159b81dfa954ea791444e4d9f4819d9
Saharco <saharco@campus.technion.ac.il>
2018-12-10 02:12:04 +0200

    Users can now upload their profile pictures to the database. Need to fetch image by default when one exists

--- app/src/main/java/com/example/yuval/takecare/UserProfileActivity.java
+++ app/src/main/java/com/example/yuval/takecare/UserProfileActivity.java
@@ -1,8 +1,19 @@
 package com.example.yuval.takecare;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,20 +22,48 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.yuval.takecare.utilities.RotateBitmap;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    FirebaseFirestore db;
-    FirebaseAuth auth;
+    private ImageView profilePictureView;
+    private ProgressBar picturePB;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private StorageReference storage;
+
+    private File selectedImageFile;
+    private Uri selectedImage;
+    private static int APP_PERMISSION_REQUEST_CAMERA;
+    private final static int REQUEST_CAMERA = 1;
+    private final static int SELECT_IMAGE = 2;
+    private final static String TAG = "MyProfile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +76,19 @@ public class UserProfileActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        profilePictureView = (ImageView) findViewById(R.id.profile_pic);
+        picturePB = (ProgressBar) findViewById(R.id.profile_pic_progress_bar);
+        selectedImageFile = null;
+        selectedImage = null;
+
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance().getReference();
+
         final TextView username = findViewById(R.id.user_name);
-        FirebaseUser user = auth.getCurrentUser();
-        if(user != null) {
+        final FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
             DocumentReference docRef = db.collection("users").document(user.getUid());
             docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                 @Override
@@ -51,6 +98,10 @@ public class UserProfileActivity extends AppCompatActivity {
                         if (document.exists()) {
                             Log.d("TAG", "DocumentSnapshot data: " + document.getData());
                             username.setText(document.getString("name"));
+                            if (document.getString("profilePicture") != null) {
+                                Log.d(TAG, "Found profile pic. Fetched picture url: " + Uri.parse(document.getString("profilePicture")));
+                                profilePictureView.setImageURI(Uri.parse(Uri.decode(document.getString("profilePicture"))));
+                            }
                         } else {
                             Log.d("TAG", "No such document");
                         }
@@ -119,5 +170,152 @@ public class UserProfileActivity extends AppCompatActivity {
                     }
                 })
                 .show();
+    }
+
+    private void setUserProfilePic(byte[] uploadBytes) {
+        final FirebaseUser user = auth.getCurrentUser();
+        assert user != null;
+        final StorageReference storageRef = storage.child("userProfilePictures/" + user.getUid());
+        UploadTask uploadTask = storageRef.putBytes(uploadBytes);
+        uploadTask
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        storageRef.getDownloadUrl()
+                                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Map<String, Object> profilePicRef = new HashMap<>();
+                                        profilePicRef.put("profilePicture", uri.toString());
+                                        db.collection("users").document(user.getUid())
+                                                .set(profilePicRef, SetOptions.merge())
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.d(TAG, "All data added successfully!");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+
+                                                    }
+                                                });
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
+    public void onChangeProfilePic(View view) {
+        PopupMenu menu = new PopupMenu(this, view);
+        menu.getMenuInflater().inflate(R.menu.photo_upload_menu, menu.getMenu());
+        menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()) {
+                    case R.id.upload_camera:
+                        if (ContextCompat.checkSelfPermission(UserProfileActivity.this, Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(UserProfileActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    APP_PERMISSION_REQUEST_CAMERA);
+                        } else {
+                            intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            selectedImageFile = new File(getExternalCacheDir(),
+                                    String.valueOf(System.currentTimeMillis()) + ".jpg");
+                            selectedImage = FileProvider.getUriForFile(UserProfileActivity.this, getPackageName() + ".provider", selectedImageFile);
+                            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, selectedImage);
+                            startActivityForResult(intent, REQUEST_CAMERA);
+                        }
+                        break;
+                    case R.id.upload_gallery:
+                        intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/*");
+                        startActivityForResult(intent, SELECT_IMAGE);
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
+        menu.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CAMERA:
+                    if (selectedImage != null) {
+                        uploadPhoto(selectedImage);
+                    }
+                    break;
+                case SELECT_IMAGE:
+                    selectedImage = data.getData();
+                    uploadPhoto(selectedImage);
+                    break;
+            }
+        }
+    }
+
+    private void uploadPhoto(Uri imagePath) {
+        ImageCompressTask resize = new ImageCompressTask();
+        resize.execute(imagePath);
+    }
+
+    public class ImageCompressTask extends AsyncTask<Uri, Integer, byte[]> {
+
+        @Override
+        protected void onPreExecute() {
+            picturePB.setVisibility(View.VISIBLE);
+            profilePictureView.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        protected byte[] doInBackground(Uri... uris) {
+            try {
+                RotateBitmap rotateBitmap = new RotateBitmap();
+                Bitmap bitmap = rotateBitmap.HandleSamplingAndRotationBitmap(UserProfileActivity.this, uris[0]);
+                Log.d(TAG, "doInBackground: MBs before compression: " + (double) bitmap.getByteCount() / 1e6);
+                byte[] bytes = getBytesFromBitmap(bitmap, 80);
+                Log.d(TAG, "doInBackground: MBs after compression: " + (double) bytes.length / 1e6);
+                return bytes;
+            } catch (IOException e) {
+                Log.d(TAG, "doInBackground: exception: " + e.getMessage());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            super.onPostExecute(bytes);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            profilePictureView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, profilePictureView.getWidth(), profilePictureView.getHeight(), false));
+            picturePB.setVisibility(View.GONE);
+            profilePictureView.setVisibility(View.VISIBLE);
+            setUserProfilePic(bytes);
+        }
+
+        private byte[] getBytesFromBitmap(Bitmap bitmap, int quality) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+            return stream.toByteArray();
+        }
     }
 }
