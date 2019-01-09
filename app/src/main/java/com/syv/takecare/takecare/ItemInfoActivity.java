df5b4d3be59dbf1d05cfcc0aa17161b675965ff3
victorbanyas <victan98@gmail.com>
2019-01-10 01:28:31 +0200

    Added fullscreen image option in Item Info

--- app/src/main/java/com/syv/takecare/takecare/ItemInfoActivity.java
+++ app/src/main/java/com/syv/takecare/takecare/ItemInfoActivity.java
@@ -1,8 +1,14 @@
 package com.syv.takecare.takecare;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.media.Image;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -22,11 +28,13 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -61,7 +69,7 @@ import java.util.Map;
 
 public class ItemInfoActivity extends AppCompatActivity {
 
-    private final static String TAG = "TakeCare";
+    private final static String TAG = "ItemInfoActivity";
     private static final String EXTRA_ITEM_ID = "Item Id";
 
     private final int imageY = 360; //Item ImageView height in dp
@@ -79,6 +87,11 @@ public class ItemInfoActivity extends AppCompatActivity {
     private AppCompatButton requestButton;
     private Button reportButton;
     private ImageButton deleteItem;
+    private View imageSpaceView;
+    private ScrollView itemInfoScrollView;
+
+    private Animator mCurrentAnimator;
+    private int mShortAnimationDuration;
 
     private CardView locationCard;
     private CardView phoneCard;
@@ -138,6 +151,8 @@ public class ItemInfoActivity extends AppCompatActivity {
         deleteItem = (ImageButton) findViewById(R.id.delete_post_button);
         requestButton = (AppCompatButton) findViewById(R.id.request_button);
         phoneCard = (CardView) findViewById(R.id.phone_card);
+        imageSpaceView = findViewById(R.id.image_space);
+        itemInfoScrollView = (ScrollView) findViewById(R.id.item_info_scroll_view);
 
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
@@ -164,7 +179,7 @@ public class ItemInfoActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                     Log.d(TAG, "user fetch: onComplete started");
                     if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
+                        final DocumentSnapshot document = task.getResult();
                         if (document.exists()) {
                             Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                             itemTitleView.setText(document.getString("title"));
@@ -196,6 +211,18 @@ public class ItemInfoActivity extends AppCompatActivity {
                                         .load(document.getString("photo"))
                                         .apply(requestOptions)
                                         .into(itemImageView);
+
+                                imageSpaceView.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        Log.d(TAG, "Image zoom onClick: Initiated");
+                                        zoomImageFromThumb(itemImageView, document);
+                                    }
+                                });
+                                // Retrieve and cache the system's default "short" animation time.
+                                mShortAnimationDuration = getResources().getInteger(
+                                        android.R.integer.config_shortAnimTime);
+
                             } else {
                                 switch(document.getString("category")) {
                                     case "Food":
@@ -612,6 +639,155 @@ public class ItemInfoActivity extends AppCompatActivity {
         snackbar.show();
     }
 
+    private void zoomImageFromThumb(final View thumbView, final DocumentSnapshot document) {
+        Log.d(TAG, "zoomImageFromThumb: Starting");
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        final ImageView expandedImageView = (ImageView) findViewById(R.id.item_image_fullscreen);
+        expandedImageView.setVisibility(View.VISIBLE);
+        itemInfoScrollView.setVisibility(View.GONE);
+        requestButton.setVisibility(View.GONE);
+        Glide.with(getApplicationContext())
+                .load(document.getString("photo"))
+                .into(expandedImageView);
+
+        Log.d(TAG, "zoomImageFromThumb: Inflated fullscreen image");
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.fullscreen_image_container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f))
+                .with(ObjectAnimator.ofFloat(expandedImageView,
+                        View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y,startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        itemInfoScrollView.setVisibility(View.VISIBLE);
+                        requestButton.setVisibility(View.VISIBLE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        itemInfoScrollView.setVisibility(View.VISIBLE);
+                        requestButton.setVisibility(View.VISIBLE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
+
+
+
+    }
 }
-
-
