6ff4b7834fbefbe95253f361e3fa10dbfebab015
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2019-01-13 19:13:02 +0200

    chat room was created

---
+++ app/src/main/java/com/syv/takecare/takecare/activities/ChatRoomActivity.java
@@ -0,0 +1,15 @@
+package com.syv.takecare.takecare.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.syv.takecare.takecare.R;
+
+public class ChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+    }
+}
